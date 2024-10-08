6ba3cab8217e1c9f952302c7a5543703bd82be64
olaf.ciula <olaf.ciula@microsoft.wsei.edu.pl>
2024-10-08 13:15:43 +0200

    Lab1

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -13,6 +13,48 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    public IActionResult Calculator(Operator? op, double? a, double? b)
+    {
+        if (a is null || b is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format liczby w parametrze a lub b";
+            return View("CustomError");
+        }
+        ViewBag.a = a;
+        ViewBag.b = b;
+
+        if (op is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny operator";
+            return View("CustomError");
+        }
+        
+        switch (op)
+        {
+            case Operator.Add:
+                ViewBag.result = a + b;
+                ViewBag.op = "+";
+                break;
+            case Operator.Sub:
+                ViewBag.result = a - b;
+                ViewBag.op = "-";
+                break;
+            case Operator.Mul:
+                ViewBag.result = a * b;
+                ViewBag.op = "*";
+                break;
+            case Operator.Div:
+                ViewBag.result = a / b;
+                ViewBag.op = ":";
+                break;
+        }
+        return View();
+    }
+    public IActionResult About()
+    {
+        return View();
+    }
+
     public IActionResult Index()
     {
         return View();
@@ -27,5 +69,10 @@ public class HomeController : Controller
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+    
+    public enum Operator
+    {
+        Add, Sub, Mul, Div
     }
 }
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f39dbb5d637ed11297ebe046f1ee8054262de28b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25cc0f3ded5dccaa9942bc70c79f35bc39c567aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
