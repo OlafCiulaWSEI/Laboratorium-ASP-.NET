cd5769047ca1f57ff52852d8e2a7f91a56b38da3
olaf.ciula <olaf.ciula@microsoft.wsei.edu.pl>
2024-10-22 13:12:52 +0200

    Add Details and Delete button

--- WebApplication1/Controllers/ContactController.cs
+++ WebApplication1/Controllers/ContactController.cs
@@ -5,7 +5,11 @@ namespace WebApplication1.Controllers;
 
 public class ContactController : Controller
 {
-    static private Dictionary<int, ContactModel> _contacts= new Dictionary<int, ContactModel>();
+    static private Dictionary<int, ContactModel> _contacts= new Dictionary<int, ContactModel>()
+    {
+        {1, new() {Id = 1, Email = "ascfsd@gmail.com",FirstName = "Olaf", LastName = "Ciuła", BirthDate = new DateTime(1970, 10,10), PhoneNumber = "333 333 333"}},
+        {2, new() {Id = 2, Email = "arfsdd@gmail.com",FirstName = "Ola", LastName = "rresgd", BirthDate = new DateTime(1970, 10,10), PhoneNumber = "333 333 333"}}
+    };
 
     private static int currentID = 0;
     // Lista kontaktów
@@ -30,5 +34,16 @@ public class ContactController : Controller
         model.Id = ++currentID;
         _contacts.Add(model.Id, model);
         return View("Index", _contacts);
+    }
+
+    public ActionResult Delete(int id)
+    {
+        _contacts.Remove(id);
+        return View("Index", _contacts);
+    }
+
+    public ActionResult Details(int id)
+    {
+        return View(_contacts[id]);
     }
 }
